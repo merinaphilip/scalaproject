b85b4a5af041eea0ac8d08bf93549ac3e1d55f24
Merin Philip <merin.a.philip@capgemini.com>
2023-10-06 18:45:02 +0530

    src and test files update

--- addNumbers.ts
+++
@@ -1,9 +0,0 @@
-function addNumbers(a: number, b: number): number {
-  return a + b;
-}
-
-const num1: number = 5;
-const num2: number = 7;
-const sum: number = addNumbers(num1, num2);
-
-console.log(`The sum of ${num1} and ${num2} is ${sum}`);

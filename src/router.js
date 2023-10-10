6d7c77e70d7bfe95da70351b1a615f99bdf0f0fc
MerinPhilip <132736333+merinaphilip@users.noreply.github.com>
2023-10-10 17:38:31 +0530

    Delete src directory

--- src/router.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const express = require('express');
-// eslint-disable-next-line new-cap
-const router = express.Router();
-const Controller = require('./controller');
-
-router.get('/posts', Controller.getPosts);
-
-module.exports = router;
